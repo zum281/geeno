63d2c064a5532372d3e702b33f23df9b90b5f50f
ZuSk <zum281@gmail.com>
2020-04-28 23:20:21 +0200

    commit 1

--- geeno.py
+++ geeno.py
@@ -1,2 +1,21 @@
-import site
-print(site.getsitepackages())
+import sys
+from PyQt5 import QtWidgets
+
+# NOW WITH CLASSES!
+
+
+class Window(QtWidgets):
+    def __init__(self):
+        super().__init__()
+        self.init_ui()
+
+    def init_ui(self):
+        self.btn1 = QtWidgets.QPushButton("Push Me")
+        self.lbl1 = QtWidgets.QLabel("I'm a label")
+
+        h_box = QtWidgets.QHBoxLayout()
+        h_box.addStretch()
+        h_box.addWidget(self.lbl1)
+        h_box.addStretch()
+
+        v_box = QtWidgets.QVBoxLayout()
